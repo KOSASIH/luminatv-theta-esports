68dca147a1d77848a6eaa452a91241a658c02805
KOSASIH <kosasihg88@gmail.com>
2024-05-09 11:45:45 +0700

    Create Home.js

---
+++ client/src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>Welcome to LuminaTV!</h1>
+    <p>The best place to watch live esports and gaming streams, chat with other fans, and participate in polls.</p>
+  </div>
+);
+
+export default Home;
